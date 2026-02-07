2466989bc64bcd33fe9d6b5ff30af66ea4e66c1f
TopTierElectric <toptierelectric11723@gmail.com>
2026-02-07 14:48:37 -0800

    feat: add aws_state main module

---
+++ terraform/modules/aws_state/main.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "state" {
+  bucket = var.bucket_name
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "lock" {
+  name         = var.lock_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.state.bucket
+}
+
+output "lock_table_name" {
+  value = aws_dynamodb_table.lock.name
+}
